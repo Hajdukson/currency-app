bba5222f8237b9400b51f6784b059fb63cd702d7
michal666 <hajduk.michal1998@gmail.com>
2022-01-15 15:09:57 +0100

    finished project

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,32 +1,41 @@
 // http://api.nbp.pl/
+import { validateValue } from "./validation.js";
 window.addEventListener("DOMContentLoaded", (e) => {
-  var loadedData;
-
-  var changeOperations = document.querySelector(".exchangeForm__operations");
-  var radios = document.querySelectorAll("input[name='operation']");
-  var exchangeForm = document.querySelector(".exchangeForm");
-  var selctions = document.getElementById("slection");
-  var finalCalculation = document.querySelector(
+  let loadedData;
+  const amount = document.getElementById("amount");
+  const changeOperations = document.querySelector(".exchangeForm__operations");
+  const radios = document.querySelectorAll("input[name='operation']");
+  const exchangeForm = document.querySelector(".exchangeForm");
+  const selctions = document.getElementById("slection");
+  const finalCalculation = document.querySelector(
     ".exchangeForm__finalCalculation"
   );
-  var resultMessage = document.querySelector(".result__text");
-  var resultValue = document.querySelector(".result__value");
-  var selectionValue;
+  const resultMessage = document.querySelector(".result__text");
+  const resultValue = document.querySelector(".result__value");
+  let selectionValue;
 
   // const api_url_tableA =
   //   "http://api.nbp.pl/api/exchangerates/tables/a/?format=json";
   // const api_url_tableB =
   //   "http://api.nbp.pl/api/exchangerates/tables/b/?format=json";
-  const api_url_tableC =
-    "https://api.nbp.pl/api/exchangerates/tables/c/?format=json";
+  const api_url_tableC = "https://api.nbp.pl/api/exchangerates/tables/c";
 
   async function getApi(url) {
-    const response = await fetch(url);
-    var data = await response.json();
-    data = data.values().next().value;
-    loadedData = data;
-    showTable(data);
-    determinValuesOfSelectTag(data);
+    try {
+      const response = await fetch(url);
+      let data = await response.json();
+      data = data.values().next().value;
+      loadedData = data;
+      showTable(data);
+      determinValuesOfSelectTag(data);
+    } catch {
+      console.log("Error occured while loading NBP API. (404 not found)");
+      let table = document.querySelector(".table");
+      let header = document.querySelector(".table__header");
+      let tableContent = document.querySelector(".table__content");
+      table.removeChild(tableContent);
+      header.textContent = "Can not load a table. 404 not found.";
+    }
   }
 
   function showTable(data) {
@@ -67,44 +76,75 @@ window.addEventListener("DOMContentLoaded", (e) => {
       selctions.append(selectionValue);
     }
 
-    if (target == "sell") {
-      resultMessage.textContent = "Otrzymasz";
-    } else if (target == "buy") {
-      resultMessage.textContent = "Musisz zapłacić";
-    }
+    resultMessage.textContent = "Średnio otrzymasz";
   }
 
-  let result = (selection, amount, e) => selection.value * amount;
+  // let result = (selection, amount, e) => selection.value * amount;
 
   function checkRadioButtons() {
     for (let i = 0; i < radios.length; i++) {
       if (radios[i].checked) {
+        let radio = radios[i].value;
         finalCalculation.hidden = false;
+        // if (target == "sell") {
+        //   amount.placeholder = "Kwota jaką chcesz wymienić";
+        // } else {
+        //   amount.placeholder = "Kwota jaką chcesz otrzymać";
+        // }
         return radios[i].value;
       }
     }
     return;
   }
 
+  var operationValue;
   changeOperations.addEventListener("change", (e) => {
     finalCalculation.hidden = false;
     resultValue.hidden = true;
     let target = e.target.value;
+    operationValue = target;
+    // if (target == "sell") {
+    //   amount.placeholder = "Kwota jaką chcesz wymienić";
+    // } else {
+    //   amount.placeholder = "Kwota jaką chcesz otrzymać";
+    // }
+    finalCalculation.hidden = false;
     determinValuesOfSelectTag(loadedData, target);
   });
 
+  function result(selectedOpr, amount, selectedCurrency) {
+    if (selectedOpr == "sell") {
+      return (amount * selectedCurrency.value).toFixed(2).toString() + " PLN";
+    }
+    let result = selectedCurrency.options[selectedCurrency.selectedIndex].text;
+    return (
+      (amount / selectedCurrency.value).toFixed(2).toString() +
+      " " +
+      result.substring(0, 3)
+    );
+  }
+
   selctions.addEventListener("change", (e) => {
-    resultMessage.hidden = true;
-    resultValue.hidden = true;
+    hideMessage();
   });
 
   exchangeForm.addEventListener("submit", (e) => {
-    let amount = document.getElementById("amount").value;
+    let value = amount.value;
+    if (!validateValue(value)) {
+      resultValue.textContent = "";
+      hideMessage();
+      return;
+    }
     resultMessage.hidden = false;
     resultValue.hidden = false;
-    console.log(document.createTextNode(result(selctions, amount, e)));
-    resultValue.textContent = result(selctions, amount, e).toFixed(2) + " PLN";
+    resultValue.textContent = result(operationValue, value, selctions);
+    // resultValue.textContent = result(selctions, value, e).toFixed(2) + " PLN";
   });
+
+  let hideMessage = () => {
+    resultMessage.hidden = true;
+    resultValue.hidden = true;
+  };
 
   var selectedRadio = checkRadioButtons();
   getApi(api_url_tableC);
---
+++ resources/js/validation.js
@@ -0,0 +1,33 @@
+const errorMessage = document.querySelector(
+  ".exchangeForm__inputAndSubmit--error"
+);
+const succes = document.getElementById("succes");
+const info = document.getElementById("error");
+const input = document.getElementById("amount");
+
+let validateValue = (value) => {
+  const val = parseFloat(value);
+  if (isNaN(val) || val === 0) {
+    errorMessage.textContent = errorHanddler("Pole nie może być puste.");
+    return false;
+  } else if (val < 0) {
+    errorMessage.textContent = errorHanddler("Kwota nie może być ujemna.");
+    return false;
+  }
+  info.style.visibility = "hidden";
+  succes.style.visibility = "visible";
+  errorMessage.style.visibility = "hidden";
+  input.classList.remove("error");
+  input.className = "succes";
+  return true;
+};
+
+let errorHanddler = (message) => {
+  info.style.visibility = "visible";
+  succes.style.visibility = "hidden";
+  input.className = "error";
+  errorMessage.style.visibility = "visible";
+  return message;
+};
+
+export { validateValue };
